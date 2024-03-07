f366b16ee3ff3d284d31922dbc4d59d28a4501c0
Naman Gupta <naman.mw4@gmail.com>
2024-03-08 00:17:55 +0530

    init

---
+++ android/app/src/main/kotlin/com/hausifyv2/hausify_v2/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.hausifyv2.hausify_v2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/utils/theme/theme.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'custom_themes/text_theme.dart';
+
+class HAppTheme {
+  HAppTheme._();
+
+  static ThemeData lightTheme = ThemeData(
+    useMaterial3: true,
+    fontFamily: 'Poppins',
+    brightness: Brightness.light,
+    primaryColor: Colors.blue,
+    scaffoldBackgroundColor: Colors.white,
+    textTheme: HTextTheme.lightTextTheme
+  );
+  static ThemeData darkTheme = ThemeData(
+      useMaterial3: true,
+      fontFamily: 'Poppins',
+      brightness: Brightness.dark,
+      primaryColor: Colors.blue,
+      scaffoldBackgroundColor: Colors.black,
+      textTheme: HTextTheme.darkTextTheme
+  );
+}
