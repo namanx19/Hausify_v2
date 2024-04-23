31409b3fb782abbefce4229cf64c5ee3017ac91a
Naman Gupta <naman.mw4@gmail.com>
2024-04-24 00:12:18 +0530

    create utils

--- lib/utils/theme/theme.dart
+++ lib/utils/theme/theme.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
-import 'custom_themes/text_theme.dart';
+import 'widget_themes/text_theme.dart';
 
-import 'custom_themes/appbar_theme.dart';
-import 'custom_themes/bottom_sheet_theme.dart';
-import 'custom_themes/checkbox_theme.dart';
-import 'custom_themes/chip_theme.dart';
-import 'custom_themes/elevated_button_theme.dart';
-import 'custom_themes/outlined_button_theme.dart';
-import 'custom_themes/text_field_theme.dart';
-import 'custom_themes/text_theme.dart';
+import 'widget_themes/appbar_theme.dart';
+import 'widget_themes/bottom_sheet_theme.dart';
+import 'widget_themes/checkbox_theme.dart';
+import 'widget_themes/chip_theme.dart';
+import 'widget_themes/elevated_button_theme.dart';
+import 'widget_themes/outlined_button_theme.dart';
+import 'widget_themes/text_field_theme.dart';
+import 'widget_themes/text_theme.dart';
 
 
 
